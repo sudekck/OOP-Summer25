338baad186d0a9a96cc6b65d6739820424fc34eb
sudekck <sudekucukoglu@gmail.com>
2025-03-06 11:46:59 +0000

    Add Student class

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ p1=Person("Sude",22,"Skierniewicka 13")
 print(p1.name)
 print(p1.age)
 print(p1.adress)
+
+class Student:
+    def __init__(self,name,last_name,index_number,nationality):
+        self.name=name
+        self.last_name=last_name
+        self.index_number=index_number
+        self.nationality=nationality
+
+s1=Student("Sude","Kucukoglu",34783,"Turkish")
+print(s1.name)
+print(s1.last_name)
+print(s1.index_number)
+print(s1.nationality)
