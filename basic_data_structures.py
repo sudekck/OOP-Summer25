f3f439ef66047c82e175c638e869507b0527b146
sudekck <sudekucukoglu@gmail.com>
2025-03-11 10:11:56 +0000

    Assign data structures to variables

---
+++ basic_data_structures.py
@@ -0,0 +1,8 @@
+my_number = 8 #int
+my_height = 1.65 #float
+name = "Sude" #str
+list_of_grades = [98,25,75] #list
+intro_of_me = {"name: "sude", "age": "22"} #dict
+index_numbers = (7654,8965,4563) #tuple
+age_of_kids = {8,10,11} #set
+is_it_open = True #bool
