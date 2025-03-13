1c6d35b24e900b271d7a69948e906e7777a3d33c
sudekck <sudekucukoglu@gmail.com>
2025-03-13 11:06:21 +0000

    Updated student details

--- student_details.py
+++ student_details.py
@@ -3,4 +3,4 @@ last_name = "Kucukoglu"
 index_number = "34783"
 nationality = "Turkish"
 starting_date = "13/09/2024"
-courses = "Basics of Cs" , "Object-Oriented Programming"
+courses = ["Basics of Cs" , "Object-Oriented Programming"]
