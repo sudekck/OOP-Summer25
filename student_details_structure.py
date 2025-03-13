bde37e904dbbfc53a922ceda2237f52f03cd06f6
sudekck <sudekucukoglu@gmail.com>
2025-03-13 11:20:41 +0000

    Add Student Details Structure

---
+++ student_details_structure.py
@@ -0,0 +1,14 @@
+student1 ={
+    "first_name": "Sude",
+    "last_name": "Kucukoglu",
+    "index_number": "34783",
+    "nationality": "Turkish",
+    "starting_date": "13/09/2024",
+    "courses": "Basics of Cs"
+}
+print(student1["first_name"])
+print(student1["last_name"])
+print(student1["index_number"])
+print(student1["nationality"])
+print(student1["starting_date"])
+print(student1["courses"])
