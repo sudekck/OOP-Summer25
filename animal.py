200c2d2df6e46e8f0daaa2e9996fdf18acec1ee0
sudekck <sudekucukoglu@gmail.com>
2025-03-20 15:01:48 +0000

    class of animals

--- animal.py
+++ animal.py
@@ -14,7 +14,7 @@ animal2={"name":"Giraffe,", "group": "Mammals,", "number_of_legs": "4", "skills"
 animal3={"name":"Crocodile,", "group":"Reptiles,", "number_of_legs": "4", "skills": "Amfibia"}
 animal4={"name":"Snake,", "group": "Reptiles,", "number_of_legs": "0,", "skills": "Poisonous"}
 animal5={"name":"Tiger,", "group":"Mammals,", "number_of_legs":"4,", "skills": "Strong"}
-print("Animals of the Zoo")
+print("Animals of the Zoo:")
 print(animal1["name"],animal1["group"],animal1["number_of_legs"],animal1["skills"])
 print(animal2["name"],animal2["group"],animal2["number_of_legs"],animal2["skills"])
 print(animal3["name"],animal3["group"],animal3["number_of_legs"],animal3["skills"])
---
+++ animal1.py
@@ -0,0 +1,35 @@
+class Animals:
+    def __init__(self,name,group,number_of_legs,skills):
+        self.name=name
+        self.group=group
+        self.number_of_legs=number_of_legs
+        self.skills=skills
+a1=Animals("Elephant","Mammals",4,"Heavy")
+print(a1.name)
+print(a1.group)
+print(a1.number_of_legs)
+print(a1.skills)
+
+a2=Animals("Giraffe","Mammals",4,"Tall")
+print(a2.name)
+print(a2.group)
+print(a2.number_of_legs)
+print(a2.skills)
+
+a3=Animals("Crocodile","Reptile",4,"Amfibia")
+print(a3.name)
+print(a3.group)
+print(a3.number_of_legs)
+print(a3.skills)
+
+a4=Animals("Snake","Reptile",0,"Poisonous")
+print(a4.name)
+print(a4.group)
+print(a4.number_of_legs)
+print(a4.skills)
+
+a5=Animals("Tiger","Mammals",4,"Wild")
+print(a5.name)
+print(a5.group)
+print(a5.number_of_legs)
+print(a5.skills)
