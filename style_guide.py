16dc01c2b79433e23142e41659be3240823d8220
sudekck <sudekucukoglu@gmail.com>
2025-04-01 12:22:51 +0000

    line lenght

--- style_guide.py
+++ style_guide.py
@@ -11,3 +11,7 @@ def function():
 if True:
 print("Indentation False") #Wrong
 
+print("2.Line Lenght:")
+
+def send_mail(subject,body,to_address):
+    print(f"Sending email to {sudekucukoglu@gmail.com}") #Correct
