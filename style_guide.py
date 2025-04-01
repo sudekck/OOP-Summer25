2faa8db2045e3605f7379afd5f1157db8fbbe010
sudekck <sudekucukoglu@gmail.com>
2025-04-01 12:14:31 +0000

    indentation

---
+++ style_guide.py
@@ -0,0 +1,13 @@
+print("Python Styling Guide:")
+#first rule code layout: indentation,line lenght and blank lines
+print("First Rule: 'Code Layout:")
+print("1.Indentation:")
+
+def function():
+    if True:
+        print("Indentation True") #Correct
+
+def function():
+if True:
+print("Indentation False") #Wrong
+
