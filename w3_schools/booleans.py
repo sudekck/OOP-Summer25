0a70b2f5a0871893c237ab0583ff787515a5a349
sudekck <sudekucukoglu@gmail.com>
2025-04-10 10:45:49 +0000

    .

---
+++ w3_schools/booleans.py
@@ -0,0 +1,23 @@
+#When you want to check an expression is whether true or false use boolean
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#try it with if loop
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#The bool() function allows you to evaluate any value, and give you True or False in return,
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
