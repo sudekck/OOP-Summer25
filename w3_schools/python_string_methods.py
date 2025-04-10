a2c792bb5ee4655031b07efd7e008cdaf9588b7c
sudekck <sudekucukoglu@gmail.com>
2025-04-10 10:29:24 +0000

    new

---
+++ w3_schools/python_string_methods.py
@@ -0,0 +1,134 @@
+#Capitalize()
+#Converts the first character to upper case
+
+#Casefold()
+#Converts string into lower case
+
+#Center()
+#Returns a centered string
+
+#Count()
+#Returns the number of times a specified value occurs in a string
+
+#Encode()
+#Returns an encoded version of the string
+
+#Endswith()
+#Returns true if the string ends with the specified value
+
+#Expandtabs()
+#Sets the tab size of the string
+
+#Find()
+#Searches the string for a specified value and returns the position of where it was found
+
+#Format()
+#Formats specified values in a string
+
+#Format_map()
+#Formats specified values in string
+
+#Index()
+#Searches the string for a specified value and returns the position of where it was found
+
+#Isalnum()
+#Returns True if all characters in the string are alphanumeric
+
+#Isalpha()
+#Returns True if all characters in the string are aplhabet characters
+
+#Isascii()
+#Returns True if all characters in the ascii characters
+
+#Isdemical()
+#Returns True if all characters in the string are decimals
+
+#Isdigit()
+#Returns True if all characters in the string are digits
+
+#Isidentifier()
+#Returns True if the string is an identifier
+
+#Islower()
+#Returns True if all characters in the string are lower case
+
+#Isnumeric()
+#Returns True if all characters in the string are numeric
+
+#Isprintable()
+#Returns True if all characters in the string are printable
+
+#Isspace()
+#Returns True if all characters in the string are whitespaces
+
+#Istitle()
+#Returns True if the string follows the rules of a title
+
+#Isupper()
+#Returns True if all characters in the string are upper case
+
+#Join()
+#Joins the elements of an iterable to the end of the string
+
+#Ljust()
+#Returns a left justified version of the string
+
+#Lower()
+#Converts a string into lower case
+
+#Lstrip()
+#Returns a left trim version of the string
+
+#Maketrans()
+#returns a translation table to be used in translations
+
+#partition()
+#returns a tuple where the string is parted into three parts
+
+#replace()
+#returns a string where a specified value is replaced with a specified value
+
+#rfind()
+#searches the string for a specified value and returns the last position of where it was found
+
+#rindex()
+#searches the string for a specified value and returns the last position of where it was found
+
+#rjust()
+#Returns a right justified version of the string
+
+#rpartition()
+#Returns a tuple where the string is parted into three parts
+
+#rsplit()
+#Splits the string at the specified separator, and returns a list
+
+#rstrip()
+#Returns a right trim version of the string
+
+#split()
+#Splits the string at the specified separator, and returns a list
+
+#splitlines()
+#Splits the string at line breaks and returns a list
+
+#startswith()
+#Returns true if the string starts with the specified value
+
+#strip()
+#Returns a trimmed version of the string
+
+#swapcase()
+#Swaps cases, lower case becomes upper case and vice versa
+
+#title()
+#Converts the first character of each word to upper case
+
+#translate()
+#Returns a translated string
+
+#upper()
+#Converts a string into upper case
+
+#zfill()
+#Fills the string with a specified number of 0 values at the beginning
