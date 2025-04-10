6d512e74ed7b106c0406b510193753ea4deae1e1
sudekck <sudekucukoglu@gmail.com>
2025-04-10 11:06:18 +0000

    s

---
+++ w3_schools/operators.py
@@ -0,0 +1,35 @@
+#Operators are used to perform operations on variables and values.
+#Arithmetic operators
+#Assignment operators
+#Comparison operators
+#Logical operators
+#Identity operators
+#Membership operators
+#Bitwise operators
+
+#Arithmetic operations
+#Addition: x+y
+#Subtraction: x-y
+#Multiplication: x*y
+#Division: x/y
+#Modulus: %
+#Exponentiation: x**y
+#Floor division: x//y
+
+#Assignment operations
+#Operator       #Example        #Same As   
+# =             # x=5           # x=5
+# +=            # x+=3          # x= x+3
+# -=            # x-=3          # x= x-3
+# *=            # x*=3          # x= x*3
+# /=            # x/=3          # x=x/3
+# %=            # x%=3          # x= x%3
+# //=           # x//=3         # x= x//3
+# **=           # x**=3         # x= x**3
+# &=            # x&=3          # x= x&3
+# |=            # x|=3          # x=x|3
+# ^=            # x^=3          # x= x^3
+# >>=           # x>>=3         # x=x>>3
+# <<=           # x<<=3         # x=x<<3
+# :=            # print(x:=3)   # x=3
+                                #print(x)
